fb466832fcc3b0a4bee0a0ecfd9532ce953ea7e6
Mohit Kumar <96984074+Mohitmk3009@users.noreply.github.com>
2025-07-28 10:46:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Header from "./components/Header";
 import ImageCarousel from "./components/ImageCarousel";
 import AboutUs from "./components/AboutUs";
